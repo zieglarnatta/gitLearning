bd50dd2733edd3f4576f14f6f3b0fad75193b32d
Roy Yap <royyap1@gmail.com>
2019-05-14 13:43:52 -0400

    I added in the IF statement and will proceed to do a PR aka Pull Request

--- play source code.rb
+++ play source code.rb
@@ -8,6 +8,15 @@ end
 class Goodbye < Hello
     def solong
       farewell = "See you later, Matz."
+      #Roy making changes here adding some random If subroutine, this is is a ruby comment by using the hash symbol
+      x = 1
+       if x > 2
+         puts "x is greater than 2"
+       elsif x <= 2 and x!=0
+         puts "x is 1"
+       else
+         puts "I can't guess the number"
+       end
       puts farewell
     end
 end
